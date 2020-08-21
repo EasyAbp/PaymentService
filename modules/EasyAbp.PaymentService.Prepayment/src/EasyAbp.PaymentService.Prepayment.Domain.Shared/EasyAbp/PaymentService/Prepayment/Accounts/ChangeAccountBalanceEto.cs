110e75fc7e97dc6b7c828493f977e001499fcd08
gdlcf88 <gdlcf88@outlook.com>
2020-08-21 17:02:53 +0800

    Add ChangeAccountBalanceEto to change account balance with distributed event

---
+++ modules/EasyAbp.PaymentService.Prepayment/src/EasyAbp.PaymentService.Prepayment.Domain.Shared/EasyAbp/PaymentService/Prepayment/Accounts/ChangeAccountBalanceEto.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using Volo.Abp.MultiTenancy;
+
+namespace EasyAbp.PaymentService.Prepayment.Accounts
+{
+    public class ChangeAccountBalanceEto : IMultiTenant
+    {
+        public Guid? TenantId { get; set; }
+
+        public Guid AccountId { get; set; }
+        
+        public decimal ChangedBalance { get; set; }
+    }
+}
---
+++ modules/EasyAbp.PaymentService.Prepayment/src/EasyAbp.PaymentService.Prepayment.Domain/EasyAbp/PaymentService/Prepayment/Accounts/ChangeAccountBalanceEventHandler.cs
@@ -0,0 +1,59 @@
+﻿using System.Threading.Tasks;
+using EasyAbp.PaymentService.Prepayment.Options.AccountGroups;
+using EasyAbp.PaymentService.Prepayment.Transactions;
+using Volo.Abp.DependencyInjection;
+using Volo.Abp.EventBus.Distributed;
+using Volo.Abp.Guids;
+using Volo.Abp.MultiTenancy;
+using Volo.Abp.Uow;
+
+namespace EasyAbp.PaymentService.Prepayment.Accounts
+{
+    public class ChangeAccountBalanceEventHandler : IDistributedEventHandler<ChangeAccountBalanceEto>, ITransientDependency
+    {
+        private readonly ICurrentTenant _currentTenant;
+        private readonly IGuidGenerator _guidGenerator;
+        private readonly IAccountRepository _accountRepository;
+        private readonly ITransactionRepository _transactionRepository;
+        private readonly IAccountGroupConfigurationProvider _accountGroupConfigurationProvider;
+
+        public ChangeAccountBalanceEventHandler(
+            ICurrentTenant currentTenant,
+            IGuidGenerator guidGenerator,
+            IAccountRepository accountRepository,
+            ITransactionRepository transactionRepository,
+            IAccountGroupConfigurationProvider accountGroupConfigurationProvider)
+        {
+            _currentTenant = currentTenant;
+            _guidGenerator = guidGenerator;
+            _accountRepository = accountRepository;
+            _transactionRepository = transactionRepository;
+            _accountGroupConfigurationProvider = accountGroupConfigurationProvider;
+        }
+        
+        [UnitOfWork(true)]
+        public async Task HandleEventAsync(ChangeAccountBalanceEto eventData)
+        {
+            using var changeTenant = _currentTenant.Change(eventData.TenantId);
+
+            var changedBalance = eventData.ChangedBalance;
+            
+            var account = await _accountRepository.GetAsync(eventData.AccountId);
+
+            var configuration = _accountGroupConfigurationProvider.Get(account.AccountGroupName);
+
+            var transactionType = changedBalance > decimal.Zero ? TransactionType.Debit : TransactionType.Credit;
+
+            var transaction = new Transaction(_guidGenerator.Create(), eventData.TenantId, account.Id, account.UserId,
+                null, transactionType, PrepaymentConsts.ChangeBalanceActionName,
+                PrepaymentConsts.ChangeBalancePaymentMethod, null, configuration.Currency, changedBalance,
+                account.Balance);
+
+            await _transactionRepository.InsertAsync(transaction, true);
+            
+            account.ChangeBalance(changedBalance);
+
+            await _accountRepository.UpdateAsync(account, true);
+        }
+    }
+}
